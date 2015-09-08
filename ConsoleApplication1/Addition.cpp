8e651e08f3274e39f4b6cb1b2689468aebd63748
sinan <sinan@arnel.co.uk>
2015-09-08 12:39:54 +0100

    addition class added

---
+++ ConsoleApplication1/Addition.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Addition.h"
+
+
+Addition::Addition()
+{
+}
+
+
+Addition::~Addition()
+{
+}
---
+++ ConsoleApplication1/Addition.h
@@ -0,0 +1,8 @@
+#pragma once
+class Addition
+{
+public:
+	Addition();
+	~Addition();
+};
+
